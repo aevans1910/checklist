dfce54d6f03a0e883f4227ed294da050733a8753
Ariane Evans <ariane.evans@student.makeschool.com>
2019-08-28 16:43:22 -0700

    added C, R, and U

--- checklist.py
+++ checklist.py
@@ -1,5 +1,16 @@
 checklist = list()
 checklist.append('Blue')
 print(checklist)
-checklist.append('Orangle')
+checklist.append('Orange')
 print(checklist)
+#Create
+def create(item):
+    checklist.append(item)
+
+#Read
+def read(index):
+    return checklist[index]
+    
+checklist = ['Blue', 'Orange']
+checklist[1] = 'Cats'
+print (checklist)
