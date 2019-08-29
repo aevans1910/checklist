5751279244f90eefe7cf56354a0ce505a0bbdde1
Ariane Evans <ariane.evans@student.makeschool.com>
2019-08-29 15:13:06 -0700

    Select and user input

--- checklist.py
+++ checklist.py
@@ -24,9 +24,43 @@ def list_all_items():
         index += 1
 
 #Marks items as completed
-def mark_completed():    
+def mark_completed():  
+    index = 0  
     for list_item in checklist:
         print ("√ {}".format(list_item))
+        index += 1
+
+#User input
+def user_input(prompt):
+    #the input function will display a message in the 
+    #terminal and wait for user input
+    user_input = input (prompt)
+    return user_input
+
+#Select
+def select (function_code):
+    #Create item
+    if function_code == "C":
+        input_item = user_input ("Input item:")
+        create(input_item)
+
+    #Read item
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+
+        read(item_index)
+
+    #Print all items
+    elif function_code == "P":
+        list_all_items()
+
+    #Catch all
+    else:
+        print("Unknown Option")
+
+        
+
+
 
 #Testing
 def test():
@@ -44,6 +78,11 @@ def test():
     list_all_items()
 
     mark_completed()
+
+    select("C")
+    list_all_items()
+    select("R")
+    list_all_items()
 
 test()
 
